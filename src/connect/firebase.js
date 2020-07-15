3b7366aaa8dd71324691ddf95d4aea326f128020
Savas <savas.erbas@mondas-gmbh.de>
2020-07-15 14:57:03 +0200

    clear connection datas for firebase

--- src/connect/firebase.js
+++ src/connect/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = firebase.initializeApp({
-  apiKey: "AIzaSyAd1UUiS0EX800E2gmCPiWGfdAEbFf_-SQ",
+  apiKey: "",
   authDomain: "react-todo-app-e791c.firebaseapp.com",
   databaseURL: "https://react-todo-app-e791c.firebaseio.com",
-  projectId: "react-todo-app-e791c",
+  projectId: "",
   storageBucket: "react-todo-app-e791c.appspot.com",
-  messagingSenderId: "705004140111",
-  appId: "1:705004140111:web:c8b983575be67ef4c85c3a",
+  messagingSenderId: "",
+  appId: "",
 });
 
 const db = firebaseConfig.firestore();
